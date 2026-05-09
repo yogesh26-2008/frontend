59d84f79e10a866246129854da0dd0a897d1da55
Liza sindhi <lizasindhi42@gmail.com>
2026-05-09 15:21:39 +0530

    hgjugggggcuohweffw bkjewiuv url

--- trandia/macos/Flutter/GeneratedPluginRegistrant.swift
+++ trandia/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import google_sign_in_ios
 import shared_preferences_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
